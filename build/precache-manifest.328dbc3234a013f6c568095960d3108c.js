14ac28f43579d79f2285955f2ae631cb3177cfec
nikhilleo <ramrakhyani94@gmail.com>
2020-09-27 14:34:12 +0530

    Done

---
+++ build/precache-manifest.328dbc3234a013f6c568095960d3108c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07e5180c9768da4e2dc703734282dc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06e0719496ed4e51cb44",
+    "url": "/static/css/main.9bb1356f.chunk.css"
+  },
+  {
+    "revision": "1a97cf3054a6756effa3",
+    "url": "/static/js/2.67d34f89.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.67d34f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e0719496ed4e51cb44",
+    "url": "/static/js/main.6998cc37.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
